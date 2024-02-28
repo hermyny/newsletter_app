ef2c13b6b59369a7f0d6e4d2239e443532875685
hermyny <hermance18@gmail.com>
2024-02-28 13:20:17 +0100

    entities

---
+++ src/Entity/Category.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'categories')]
+    private Collection $subscriber;
+
+    #[ORM\OneToMany(targetEntity: Newsletter::class, mappedBy: 'category')]
+    private Collection $newsletters;
+
+    public function __construct()
+    {
+        $this->subscriber = new ArrayCollection();
+        $this->newsletters = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getSubscriber(): Collection
+    {
+        return $this->subscriber;
+    }
+
+    public function addSubscriber(User $subscriber): static
+    {
+        if (!$this->subscriber->contains($subscriber)) {
+            $this->subscriber->add($subscriber);
+        }
+
+        return $this;
+    }
+
+    public function removeSubscriber(User $subscriber): static
+    {
+        $this->subscriber->removeElement($subscriber);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Newsletter>
+     */
+    public function getNewsletters(): Collection
+    {
+        return $this->newsletters;
+    }
+
+    public function addNewsletter(Newsletter $newsletter): static
+    {
+        if (!$this->newsletters->contains($newsletter)) {
+            $this->newsletters->add($newsletter);
+            $newsletter->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNewsletter(Newsletter $newsletter): static
+    {
+        if ($this->newsletters->removeElement($newsletter)) {
+            // set the owning side to null (unless already changed)
+            if ($newsletter->getCategory() === $this) {
+                $newsletter->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?bool $is_rgpd = false;
+
+    #[ORM\Column(length: 255)]
+    private ?string $validation_token = null;
+
+    #[ORM\Column]
+    private ?bool $is_valid = false;
+
+    #[ORM\ManyToMany(targetEntity: Category::class, mappedBy: 'subscriber')]
+    private Collection $categories;
+
+   
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTimeImmutable();
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function isIsRgpd(): ?bool
+    {
+        return $this->is_rgpd;
+    }
+
+    public function setIsRgpd(bool $is_rgpd): static
+    {
+        $this->is_rgpd = $is_rgpd;
+
+        return $this;
+    }
+
+    public function getValidationToken(): ?string
+    {
+        return $this->validation_token;
+    }
+
+    public function setValidationToken(string $validation_token): static
+    {
+        $this->validation_token = $validation_token;
+
+        return $this;
+    }
+
+    public function isIsValid(): ?bool
+    {
+        return $this->is_valid;
+    }
+
+    public function setIsValid(bool $is_valid): static
+    {
+        $this->is_valid = $is_valid;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->addSubscriber($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removeSubscriber($this);
+        }
+
+        return $this;
+    }
+
+
+   
+
+}
